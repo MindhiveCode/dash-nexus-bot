86ae85eca32a12fc629735467ef2c87758c3764f
Jeff Hodges - Desktop <jeffhodges93@gmail.com>
2018-08-09 14:04:21 -0700

    Changed method for getting the latest.json to just use Requests since the S3 bucket is open anyways

--- lib/diff_checker.py
+++ lib/diff_checker.py
@@ -2,7 +2,6 @@ import requests
 import json
 import tinys3
 import boto
-from boto.s3.key import Key
 import os
 
 # bucket = 'dash-nexus-bot'
@@ -44,9 +43,21 @@ def get_old():
         return data
 
 
+def get_old_2():
+    try:
+        response = requests.get("https://s3.us-east-2.amazonaws.com/nexus-test-bucket-hodges/latest.json")
+        if response.status_code == 200:
+            return response.json()
+    except Exception as e:
+        print("Error: {}".format(e))
+        data = get_new()
+        upload_new(data)
+        return data
+
+
 def get_both():
     new = get_new()
-    old = get_old()
+    old = get_old_2()
 
     return new, old
 
@@ -93,24 +104,30 @@ def upload_new(budget_data):
 
 
 def check_for_new():
+    print("Getting old data and fresh data")
     new, old = get_both()
+
+    print("Generating list of proposal hashes from both")
     new_hashes = parse_hashes(new)
     old_hashes = parse_hashes(old)
 
+    print("Generating diff list")
     diff = list(set(new_hashes) - set(old_hashes))
 
     if len(diff) > 0:
-        print("Send message")
+        print("New proposal detected")
+        print("Sending message...")
 
         # Iterate over proposals, print (mock doing something) for the newly added proposals.
         for proposal in new['proposals']:
             if proposal['hash'] in diff:
                 print(proposal)
 
+        print("Uploading new data")
         if upload_new(new):
             print("Successfully uploaded file.")
         else:
-            print("Failed to cache latest proposal data.")
+            print("Failed to uploaded latest proposal data.")
     else:
         print("No new proposals. Waiting a bit.")
 
